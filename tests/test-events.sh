02134ef452360e36a4ba2b5f98e6378d28185a6a
E. Madison Bray <erik.m.bray@gmail.com>
2025-06-12 13:15:27 +0200

    test: add sample `asdf events` outputs for complex.asdf and int.asdf
    these files are failing on macOS for some reason; this might give more
    insight than `asdf info` gh-16

--- tests/test-events.sh
+++ tests/test-events.sh
@@ -6,5 +6,7 @@ fi
 
 ./shell-test.sh events --verbose $@ \
   ${SRCDIR}/asdf-standard/reference_files/1.6.0/basic.asdf \
-  ${SRCDIR}/asdf-standard/reference_files/1.6.0/compressed.asdf
+  ${SRCDIR}/asdf-standard/reference_files/1.6.0/complex.asdf \
+  ${SRCDIR}/asdf-standard/reference_files/1.6.0/compressed.asdf \
+  ${SRCDIR}/asdf-standard/reference_files/1.6.0/int.asdf
 
