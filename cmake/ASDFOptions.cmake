a054a4f20103bb6aed350f6f7ce543a9e388c6b8
Joseph Hunkeler <jhunkeler@gmail.com>
2025-09-19 10:54:56 -0400

    Break main cmake script into easier to manage pieces

---
+++ cmake/ASDFOptions.cmake
@@ -0,0 +1,26 @@
+# Configure options for ASDF library
+include(AddressAnalyzer)
+
+if(ASDF_LOG_ENABLED)
+    option(ASDF_LOG_COLOR "Enable colored log output" ON)
+endif()
+set(ASDF_DEFAULT_LOG_LEVEL TRACE CACHE STRING "One of TRACE|DEBUG|INFO|WARN|ERROR|FATAL|NONE")
+set(ASDF_LOG_MIN_LEVEL TRACE CACHE STRING "One of TRACE|DEBUG|INFO|WARN|ERROR|FATAL|NONE")
+
+# Testing
+option(ENABLE_TESTING OFF)
+option(ENABLE_TESTING_SHELL OFF)
+
+# Distribution
+set(CPACK_PACKAGE_VENDOR "STScI")
+set(CPACK_SOURCE_GENERATOR "TGZ")
+set(CPACK_SOURCE_IGNORE_FILES
+        \\.git/
+        \\.github/
+        \\.idea/
+        "cmake-.*/"
+        build/
+        ".*~$"
+)
+set(CPACK_VERBATIM_VARIABLES YES)
+include(CPack)
