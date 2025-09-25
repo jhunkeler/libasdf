75ffb4efc7fd59b40e846b133a9968bd9f8e7bd2
E. Madison Bray <erik.m.bray@gmail.com>
2025-09-25 13:38:14 +0200

    test: for posterity check in some code used to generate some of the test files
    (The test files not included in here were all written by hand in fact,
    and just contain headers)

---
+++ tests/scripts/make_test_fixtures.py
@@ -0,0 +1,108 @@
+"""
+This file contains code for generating many of the test files in this
+repository.
+
+The files are checked in to the repository for now as they as small, though
+this could also be used if we want to generate some larger test files later.
+
+It is stored here for reference, and requires the asdf Python library, numpy,
+etc.
+"""
+
+import argparse
+import sys
+from pathlib import Path
+
+import asdf
+import numpy as np
+
+
+def make_255_asdf():
+    f = asdf.AsdfFile()
+    f['data'] = np.arange(256, dtype=np.dtype('>u1'))
+    f.add_history_entry(
+        'test file containing integers from 0 to 255 in the block '
+        'data, for simple tests against known data')
+    return f
+
+
+def make_byteorder_asdf():
+    f = asdf.AsdfFile()
+    data = np.arange(8)
+    f['uint16-big'] = data.astype('>u2')
+    f['uint16-little'] = data.astype('<u2')
+    f['uint32-big'] = data.astype('>u4')
+    f['uint32-little'] = data.astype('<u4')
+    f['uint64-big'] = data.astype('>u8')
+    f['uint64-little'] = data.astype('<u8')
+    return f
+
+
+def make_cube_asdf():
+    f = asdf.AsdfFile()
+    f['cube'] = np.arange(10 ** 3, dtype=np.dtype('>u1')).reshape((10, 10, 10))
+    f.add_history_entry('A very small data cube for testing')
+    return f
+
+
+def make_tiles_asdf():
+    f = asdf.AsdfFile()
+
+    # Mix of different data types for each array
+    dtypes = [
+        np.dtype('float64'),
+        np.dtype('>u1'),
+        np.dtype('<u2'),
+        np.dtype('<i4')
+    ]
+
+    f['0d'] = np.array([], dtype=dtypes[0])
+
+    for ndim in range(1, len(dtypes)):
+        shape = (4,) * ndim
+        idx = np.indices(shape)
+        factors = 10 ** np.arange(
+            len(shape) - 1, -1, -1,
+        ).reshape(-1, *((1,) * len(shape)))
+        array = np.sum((idx + 1) * factors, axis=0, dtype=dtypes[ndim])
+        f[f'{ndim}d'] = array
+
+    return f
+
+
+TEST_FILES = {
+    '255.asdf': make_255_asdf,
+    'byteorder.asdf': make_byteorder_asdf,
+    'cube.asdf': make_cube_asdf,
+    'tiles.asdf': make_tiles_asdf,
+}
+"""Maps the names of test files to the function that generates it"""
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description='Generate test fixture files')
+    parser.add_argument('filename', nargs='?', type=str,
+                        help='The name of the test file to generate; if '
+                             'omitted lists the names of the file this script '
+                             'can generate')
+    parser.add_argument('--fixtures-dir', type=Path,
+                        default=Path('../fixtures'),
+                        help='Path to the test fixtures directory')
+    args = parser.parse_args(argv)
+
+    if not args.filename:
+        for filename in TEST_FILES:
+            print(filename)
+
+        return 0
+
+    if args.filename not in TEST_FILES:
+        parser.error(f'unknown test file: {args.filename}')
+
+    asdf_file = TEST_FILES[args.filename]()
+    asdf_file.write_to(args.fixtures_dir / args.filename)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
