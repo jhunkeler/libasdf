f319338bbcc82d1b9980a31c2d3e06837dc7abaf
E. Madison Bray <madison@bray.work>
2026-04-02 15:08:14 +0200

    refactor: delete the gwcs-specific sources
    They are moved into libasdf-gwcs now

--- src/gwcs/gwcs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define ASDF_GWCS_INTERNAL
-#include "asdf/gwcs/core.h"                             // IWYU pragma: export
-#include "asdf/gwcs/fitswcs_imaging.h"                  // IWYU pragma: export
-#include "asdf/gwcs/frame.h"                            // IWYU pragma: export
-#include "asdf/gwcs/frame2d.h"                          // IWYU pragma: export
-#include "asdf/gwcs/frame_celestial.h"                  // IWYU pragma: export
-#include "asdf/gwcs/transforms/property/bounding_box.h" // IWYU pragma: export
