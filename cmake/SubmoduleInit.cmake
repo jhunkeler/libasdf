7c91beaf34ba73fb5eb9760cc606808adab4c56d
E. Madison Bray <676149+embray@users.noreply.github.com>
2026-04-03 14:59:21 +0200

    build: fix cmake submodule init subroutine
    execute_process doesn't run a subshell so passing it shell commands is
    meaningless.  Use WORKING_DIRECTORY instead and also catch errors;
    output messages as STATUS messages

--- cmake/SubmoduleInit.cmake
+++ cmake/SubmoduleInit.cmake
@@ -1,15 +1,21 @@
 function(submodule_init)
-    foreach (path IN ITEMS ${ARGV0})
-        message("Update submodule: ${path}")
+    foreach (path IN LISTS ARGV)
+        message(STATUS "Updating submodule: ${path}")
+
         execute_process(
-                COMMAND "git config get submodule.${path}.ignore"
-                RESULT_VARIABLE ignored
+            COMMAND git submodule update --init ${path}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            RESULT_VARIABLE result
+            ERROR_VARIABLE error
         )
+
+        if(NOT result EQUAL 0)
+            message(FATAL_ERROR "Failed to update submodule ${path}:\n${error}")
+        endif()
+
         execute_process(
-                COMMAND "cd ${CMAKE_SOURCE_DIR} && git submodule update --init ${path}"
-        )
-        execute_process(
-                COMMAND "cd ${CMAKE_SOURCE_DIR} && git config submodule.${path}.ignore untracked"
+            COMMAND git config submodule.${path}.ignore untracked
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         )
     endforeach()
 endfunction()
